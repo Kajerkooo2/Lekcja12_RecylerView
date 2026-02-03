83d132beb101b0dd722d0e231889e7c179e62a91
Kajerkooo2 <prokajerko79@gmail.com>
2026-02-03 09:01:23 +0100

    MainActiity podlaczenie wszystkie,odpalanie aplikacji

--- app/src/main/java/com/example/recylerview/MainActivity.java
+++ app/src/main/java/com/example/recylerview/MainActivity.java
@@ -1,24 +1,50 @@
 package com.example.recylerview;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<Produkt> produkty = new ArrayList<>();
+    private RecyclerView recyclerView;
+    private ZakupyAdapter zakupyAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+        // EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        produkty.add(new Produkt("maslo",5.75));
+        produkty.add(new Produkt("parowki",3.75));
+        produkty.add(new Produkt("woda",2));
+        produkty.add(new Produkt("Dubai Choclate",55.55));
+        produkty.add(new Produkt("Coca-cola",9.80));
+        produkty.add(new Produkt("Chips Lays Zielona Cebulka",7.99));
+        produkty.add(new Produkt("Trunk dla doroslych",49.99));
+        produkty.add(new Produkt("chleb",3.99));
+
+        recyclerView = findViewById(R.id.recyclerView);
+        zakupyAdapter = new ZakupyAdapter(MainActivity.this, produkty);
+        recyclerView.setAdapter(zakupyAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
     }
 }
