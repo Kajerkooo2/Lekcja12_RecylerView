81fa0e0c6ee171cec92c163809a9d09cc7d9d4a7
Kajerkooo2 <prokajerko79@gmail.com>
2026-02-03 09:14:05 +0100

    Dodawanie produktu

--- app/src/main/java/com/example/recylerview/MainActivity.java
+++ app/src/main/java/com/example/recylerview/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.recylerview;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -19,6 +20,10 @@ public class MainActivity extends AppCompatActivity {
     private ArrayList<Produkt> produkty = new ArrayList<>();
     private RecyclerView recyclerView;
     private ZakupyAdapter zakupyAdapter;
+    private Button buttonDodaj;
+
+    private EditText editTextCena, editTextNazwa;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,5 +51,22 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setAdapter(zakupyAdapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
+        buttonDodaj = findViewById(R.id.buttonDodaj);
+        editTextNazwa = findViewById(R.id.editTextNazwa);
+        editTextCena = findViewById(R.id.editTextCena);
+
+        buttonDodaj.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String nazwaProduktu = editTextNazwa.getText().toString();
+                double cena = Double.parseDouble((editTextCena.getText().toString()));
+                produkty.add(new Produkt(nazwaProduktu,cena));
+
+                zakupyAdapter.notifyDataSetChanged();
+                editTextCena.setText("");
+                editTextNazwa.setText("");
+            }
+
+        });
     }
 }
